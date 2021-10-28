5469881a8fa07579eba0b34e2c47b7bbc475ad66
Ian Boyle <irboyle21@gmail.com>
2021-10-28 08:52:47 -0400

    adds user and sessions routes, controllers, models, and create requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/actors" => "actors#index"
+  
+  ###MOVIE ROUTES
   get "/movies" => "movies#index"
   get "movies/:id" => "movies#show"
-  get "/actors/:id" => "actors#show"
-  post "/actors" => "actors#create"
   post "/movies" => "movies#create"
   patch "/movies/:id" => "movies#update"
-  patch "/actors/:id" => "actors#update"
   delete "/movies/:id" => "movies#destroy"
+  
+
+  ###ACTOR ROUTES
+  get "/actors" => "actors#index"
+  get "/actors/:id" => "actors#show"
+  post "/actors" => "actors#create"
+  patch "/actors/:id" => "actors#update"
   delete "/actors/:id" => "actors#destroy"
+
+  ###USER ROUTES
+  post "/users" => "users#create"
+
+  ###SESSION ROUTES
+  post "/sessions" => "sessions#create"
 end
