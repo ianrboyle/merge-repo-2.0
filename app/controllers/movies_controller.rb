60905a1e31f0ced8b701e36f3b13a0517e6d0eaf
Ian Boyle <irboyle21@gmail.com>
2021-10-29 11:51:47 -0400

    makes movie serializer

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,18 +4,18 @@ class MoviesController < ApplicationController
   
   def index
     movies = Movie.all
-    render json: {movies: movies.as_json}
+    render json: movies
   end
 
   def show
     movie = Movie.find_by(id: params["id"])
-    render json: movie.as_json
+    render json: movie
   end
 
   def create
     movie = Movie.new(title: params["title"], year: params["year"], plot: params["plot"], director: params["director"], english: params["english"])
     if movie.save
-      render json: movie.as_json
+      render json: movie
     else
       render json: {errors: movie.errors.full_messages}, status: 418
     end
@@ -29,7 +29,7 @@ class MoviesController < ApplicationController
     movie.director = params["director"] || movie.director
     movie.english = params["english"] || movie.english
     if movie.save
-      render json: movie.as_json
+      render json: movie
     else
       render json: {errors: movie.errors.full_messages}, status: 418
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,7 @@ class Movie < ApplicationRecord
   has_many :movie_genres
   has_many :genres, through: :movie_genres
 
+
   # has_many :genres, through: :movie_genre
   # validates :title, presence: true
   # validates :director, presence: true
