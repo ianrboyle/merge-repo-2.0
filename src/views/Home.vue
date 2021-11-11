6a159ee5a333581d80467914ec5fc8f36825e035
Ian Boyle <irboyle21@gmail.com>
2021-11-11 14:02:14 -0500

    creates login, signup, logout routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>{{ message }}</h1>
-    <div v-for="post in posts" :key="post.id">
-      <h1>{{ post }}</h1>
-    </div>
+
     <!-- Post Title:
     <input type="text" v-model="postTitle" />
     Post Content:
@@ -15,29 +13,12 @@
 
 <script>
 // @ is an alias to /src
-import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome to Ian's Blog!",
-      posts: [],
-      // newRecipeParams: {},
-      // currentRecipe: {},
     };
-  },
-  created: {
-    function() {
-      this.getPosts();
-    },
-  },
-  methods: {
-    getPosts: function () {
-      axios.get("http://localhost:3000/posts").then((response) => {
-        console.log("Success", response.data);
-        this.posts = response.data;
-      });
-    },
-    createPost: function () {},
   },
 };
 </script>
--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="posts">
     <h1>This is a posts page</h1>
+    <div v-for="post in posts" :key="post.id">
+      <h5>{{ post.title }}</h5>
+      <button v-on:click=""></button>
+      <dialog id="poost-details">
+        <form method="dialog">
+          <h1>Post Info</h1>
+        </form>
+      </dialog>
+    </div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Ian's Blog!",
+      posts: [],
+    };
+  },
+  created: function () {
+    this.getPosts();
+  },
+
+  methods: {
+    getPosts: function () {
+      axios.get("http://localhost:3000/posts").then((response) => {
+        this.posts = response.data;
+        console.log("Success", response.data);
+      });
+    },
+    showPost: function (post) {},
+    // createPost: function () {},
+  },
+};
+</script>
