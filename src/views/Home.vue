c63cde5e6dc1c5331a9f3ba87704fa349a4854f7
Ian Boyle <irboyle21@gmail.com>
2021-11-11 12:53:44 -0500

    install axios. creates posts page/route. attempts to display posts on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div v-for="post in posts" :key="post.id">
+      <h1>{{ post }}</h1>
+    </div>
+    <!-- Post Title:
+    <input type="text" v-model="postTitle" />
+    Post Content:
+    <input type="text" v-model="postContent" /> -->
+    <!-- <button v-on:click="createPost()">Create New Post</button> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Ian's Blog!",
+      posts: [],
+      // newRecipeParams: {},
+      // currentRecipe: {},
+    };
+  },
+  created: {
+    function() {
+      this.getPosts();
+    },
+  },
+  methods: {
+    getPosts: function () {
+      axios.get("http://localhost:3000/posts").then((response) => {
+        console.log("Success", response.data);
+        this.posts = response.data;
+      });
+    },
+    createPost: function () {},
   },
 };
 </script>
