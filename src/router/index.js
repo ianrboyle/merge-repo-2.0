c87749688fbb9aeaedc5ef9ce53189ff0da2df76
Ian Boyle <irboyle21@gmail.com>
2021-11-15 12:50:16 -0500

    adds postshow route

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
 import Logout from "../views/Logout.vue";
 import PostsNew from "../views/PostsNew.vue";
+import PostsShow from "../views/PostsShow.vue";
 
 Vue.use(VueRouter);
 
@@ -32,6 +33,7 @@ const routes = [
   { path: "/login", name: "login", component: Login },
   { path: "/logout", name: "logout", component: Logout },
   { path: "/posts/new", name: "PostsNew", component: PostsNew },
+  { path: "/posts/1", name: "PostsShow", component: PostsShow },
 ];
 
 const router = new VueRouter({
---
+++ src/views/PostsShow.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="posts-show">
+    <h1>This is an individual post's page</h1>
+    <h3>{{ post.title }}</h3>
+    <img :src="post.image" :alt="post.title" />
+    <br />
+    <p>{{ post.body }}</p>
+    <router-link to="/">Home Page</router-link>
+  </div>
+</template>
+<style>
+img {
+  width: 250px;
+}
+</style>
+<script>
+// @ is an alias to /src
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      post: {},
+    };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/posts/1").then((response) => {
+      this.post = response.data;
+      console.log("Success", response.data);
+    });
+  },
+
+  methods: {},
+};
+</script>
