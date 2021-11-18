48772548198459cbaec4942435d2d06476a447f4
Ian Boyle <irboyle21@gmail.com>
2021-11-18 12:55:43 -0500

    adds isLoggedIn function

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
-      |
-      <router-link to="/posts">Posts</router-link>
-      |
-      <router-link to="/signup">Signup</router-link>
-      |
-      <router-link to="/login">Login</router-link>
-      |
-      <router-link to="/logout">Logout</router-link>
-      |
-      <router-link to="/posts/new">New Post</router-link>
-    </div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="/">Home Page</a>
         <button
           class="navbar-toggler"
           type="button"
@@ -32,10 +17,22 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link active" aria-current="page" href="/about">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+              <a class="nav-link" href="/posts">All Posts</a>
+            </li>
+            <li v-if="isLoggedIn()" class="nav-item">
+              <a class="nav-link" href="/posts/new">Create New Post</a>
+            </li>
+            <li v-if="!isLoggedIn()" class="nav-item">
+              <a class="nav-link" href="/signup">Signup</a>
+            </li>
+            <li v-if="!isLoggedIn()" class="nav-item">
+              <a class="nav-link" href="/login">Login</a>
+            </li>
+            <li v-if="isLoggedIn()" class="nav-item">
+              <a class="nav-link" href="/logout">Logout</a>
             </li>
             <li class="nav-item dropdown">
               <a
@@ -49,7 +46,7 @@
                 Dropdown
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Logout</a></li>
                 <li><a class="dropdown-item" href="#">Another action</a></li>
                 <li><hr class="dropdown-divider" /></li>
                 <li><a class="dropdown-item" href="#">Something else here</a></li>
@@ -89,3 +86,17 @@ small {
   color: white;
 }
 </style>
+
+<script>
+export default {
+  methods: {
+    isLoggedIn: function () {
+      if (localStorage.getItem("jwt")) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+</script>
